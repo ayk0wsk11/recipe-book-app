84bc41623ec6fa903dca147785fee428c9f18e0f
Jonathan Senf <jonathansenf97@gmail.com>
2024-05-28 17:01:05 +0200

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <div>
+      <h4>Footer</h4>
+    </div>
+  );
+};
+export default Footer;
